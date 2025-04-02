f8f4cb7d5ee4928df6985e70a9a1795ff07043e5
이상용 <lsy3709@naver.com>
2025-04-02 10:37:15 +0900

    2교시, 공공데이터 연동 준비 작업2, 화면 작업 ,Consumer 작업, 리스트 뷰작업, 각 아이템의 요소를 제목, 이미지, 부제목등을 카드 레이아웃에 할당 작업

--- lib/sample_routing/pd_test_screen/my_pd_test_screen.dart
+++ lib/sample_routing/pd_test_screen/my_pd_test_screen.dart
@@ -1,4 +1,6 @@
+import 'package:busanit501_flutter_test/sample_routing/pd_test_controller/food_controller.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MyPdTestScreen extends StatelessWidget {
@@ -8,6 +10,41 @@ class MyPdTestScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // Provider 패턴을 이용해서, 데이터를 받아오면, 화면에 연동하는 작업,
     return ChangeNotifierProvider(
-        create: create)
+      create: (_) => FoodController()..fetchFoodData(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('부산 맛집 정보 서비스')),
+        body: Consumer<FoodController>(
+          builder: (context, controller, _) {
+            if (controller.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (controller.items.isEmpty) {
+              return const Center(child: Text('데이터가 없습니다.'));
+            }
+            return ListView.builder(
+              itemCount: controller.items.length,
+              itemBuilder: (context, index) {
+                final item = controller.items[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    leading:
+                        item.image != null
+                            ? Image.network(
+                              item.image!,
+                              width: 80,
+                              fit: BoxFit.cover,
+                            )
+                            : const Icon(Icons.image_not_supported),
+                    title: Text(item.mainTitle ?? ""),
+                    subtitle: Text(item.title ?? ""),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
