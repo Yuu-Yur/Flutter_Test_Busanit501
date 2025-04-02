b7e84827bbd87a49836dd0121390b2d9a57ebda4
이상용 <lsy3709@naver.com>
2025-04-02 10:40:28 +0900

    2교시, 공공데이터 연동 준비 작업3, 메인에 프로바이더 등록 작업.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:busanit501_flutter_test/sample_routing/login_screen.dart';
 import 'package:busanit501_flutter_test/sample_routing/my_app_routing.dart';
 import 'package:busanit501_flutter_test/sample_routing/my_splash_screen.dart';
+import 'package:busanit501_flutter_test/sample_routing/pd_test_controller/food_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
   // runApp(MyApp());
-  runApp(MyAppRouting());
+  // 프로바이더 등록 작업,
+  // runApp(MyAppRouting());
+  runApp(
+    MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => FoodController())
+        ],
+    child: const MyAppRouting(),)
+  );
 }
 
 
